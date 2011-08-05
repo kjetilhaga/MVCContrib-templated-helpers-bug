c663e03fa3a0a3358f4c7f506698a72fd06fdc97
Kjetil <kjetil.haga@gmail.com>
2011-08-05 23:00:09 +0200

    Removed redundant usings

--- MVCContrib-templated-helpers-bug/Controllers/HomeController.cs
+++ MVCContrib-templated-helpers-bug/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using MVCContrib_templated_helpers_bug.Models;
 
 namespace MVCContrib_templated_helpers_bug.Controllers
--- MVCContrib-templated-helpers-bug/Models/UserInfo.cs
+++ MVCContrib-templated-helpers-bug/Models/UserInfo.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace MVCContrib_templated_helpers_bug.Models
+﻿namespace MVCContrib_templated_helpers_bug.Models
 {
     public class UserInfo
     {
